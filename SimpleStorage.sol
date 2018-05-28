b980fbed20bb837fad1f38a29cb7cb65b17cb358
Ayuba Muazu <35365191+yubzee@users.noreply.github.com>
2018-05-28 02:10:59 -1200

    Add files via upload

---
+++ SimpleStorage.sol
@@ -0,0 +1,10 @@
+﻿pragma solidity ^0.4.4;
+contract SimpleStorage{
+uint storeddata;
+function set(uint x) public{
+storeddata = x;
+}
+function get() public view returns(uint){
+return storeddata;
+}
+}
